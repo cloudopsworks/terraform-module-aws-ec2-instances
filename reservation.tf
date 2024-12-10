d50e46662b5f5bb104647798338b6c8f708a3566
Cristian Beraha <cristian@cloudops.works>
2024-12-10 10:58:24 -0300

    chore: fix instance family

--- reservation.tf
+++ reservation.tf
@@ -8,7 +8,7 @@ resource "aws_ec2_host" "this" {
   count             = try(var.instance.create, true) && !try(var.instance.create_spot, false) && try(var.instance.dedicated_host.enabled, false) ? 1 : 0
   availability_zone = try(var.instance.availability_zone, null)
   instance_family   = try(var.instance.dedicated_host.instance_family, null)
-  instance_type     = try(var.instance.dedicated_host.instance_family, "") != "" ? var.instance.type : null
+  instance_type     = try(var.instance.dedicated_host.instance_family, "") == "" ? var.instance.type : null
   host_recovery     = try(var.instance.dedicated_host.host_recovery, null)
   auto_placement    = try(var.instance.dedicated_host.auto_placement, null)
   tags              = local.all_tags
